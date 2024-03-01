7661194c3d3883268668ada60f3c58ea2e9ae01f
sainiRJ <rajeshpushpakar01@gmial.com>
2024-03-01 18:33:12 +0530

    feat: apis for add product

--- src/components/Products/ShopProduct.js/ShopProduct.js
+++ src/components/Products/ShopProduct.js/ShopProduct.js
@@ -1,35 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const ShopProduct = ({ product }) => {
-  const handleAddToCart = (product) => {
-    const token = localStorage.getItem('token');
-
-    axios.post('http://localhost:5001/addcart', product,{
-      headers: {
-        'Content-Type': 'multipart/form-data',
-        Authorization: `Bearer ${token}`,
-      },
-    })
-      .then(response => {
-        console.log('Product added to cart:', response.data);
-      })
-      .catch(error => {
-        console.error('Error adding product to cart:', error);
-      });
-  };
+const ShopProduct = ({ product, addToCart }) => {
   return (
     <div className="col-lg-4 col-md-4 col-sm-6 pb-1">
       <div className="product-item bg-light mb-4">
         <div className="product-img position-relative overflow-hidden" style={{ height: '300px'}}>
           <img
-            className="img-fluid w-100 h-100"
+            className="img-fluid w-100 h-100" 
             src={product.image}
             alt={product.title}
             style={{ objectFit: 'contain', width: '150px', height: '200px' }}
           />
           <div className="product-action">
-            <a className="btn btn-outline-dark btn-square" href="" onClick={handleAddToCart(product)}><i className="fa fa-shopping-cart"></i></a>
+            <a className="btn btn-outline-dark btn-square" onClick={() => addToCart(product)}><i className="fa fa-shopping-cart"></i></a>
             <a className="btn btn-outline-dark btn-square" href=""><i className="far fa-heart"></i></a>
             <a className="btn btn-outline-dark btn-square" href=""><i className="fa fa-sync-alt"></i></a>
             <a className="btn btn-outline-dark btn-square" href=""><i className="fa fa-search"></i></a>
@@ -54,23 +38,41 @@ const ShopProduct = ({ product }) => {
 
 const ShopProducts = () => {
   const [products, setProducts] = useState([]);
-
+  
   useEffect(() => {
     // Fetch data from the backend using Axios
     axios.get("http://localhost:5001/products")
       .then(response => {
         // Assuming the response data is an array of products
         setProducts(response.data);
-        console.log("this is data" + response.data)
+        console.log("response" + response.data)
       })
       .catch(error => {
         console.error('Error fetching data:', error);
       });
   }, []);
 
+  const handleAddToCart = product => {
+    const token = localStorage.getItem('token');
+    console.log("prod: ", product);
+
+    axios.post('http://localhost:5001/product',product,{
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then(response => {
+        console.log('Product added to cart:', response.data);
+      })
+      .catch(error => {
+        console.error('Error adding product to cart:', error);
+      });
+  };
+
   return (
     <div className="row px-xl-5">
-      {products.map(product => <ShopProduct key={product.id} product={product} />)}
+      {products.map(product => <ShopProduct key={product.id} product={product} addToCart={handleAddToCart} />)}
     </div>
   );
 };
